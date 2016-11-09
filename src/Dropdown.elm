b63f28deb2846cddb137d119709d798e76bcc778
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2016-11-08 18:15:23 -0600

    Add dropdown component

---
+++ src/Dropdown.elm
@@ -0,0 +1,93 @@
+module Dropdown exposing (dropdown, Item, Options, defaultOptions)
+
+{-| Dropdown
+
+Options
+@docs Item, Options, defaultOptions
+
+# View
+@docs dropdown
+-}
+
+import Html exposing (select, option, Html)
+import Html.Attributes as Html
+import Html.Events exposing (on, targetValue)
+import Json.Decode as Decode
+
+
+{-| Item is the individual content of the dropdown.
+
+ * `value` is the item value or `id`
+ * `text` is the display text of the dropdown item.
+ * `enabled` is a flag to indicate whether the item is enabled or disabled.
+-}
+type alias Item =
+    { value : String, text : String, enabled : Bool }
+
+
+{-| Options for the dropdown.
+
+ * `items` is content of the dropdown.
+ * `emptyItem` is the item for when the nothing is selected. Set to `Nothing` for no empty item.
+ * `onChange` is the message for when the selected value in the dropdown is changed.
+-}
+type alias Options msg =
+    { items : List Item, emptyItem : Maybe Item, onChange : Maybe String -> msg }
+
+
+{-| Default Options, will give you empty dropdown with no empty item
+-}
+defaultOptions : (Maybe String -> msg) -> Options msg
+defaultOptions onChange =
+    { items = [], emptyItem = Nothing, onChange = onChange }
+
+
+{-| Html element.
+
+Put this in your view's Html content.
+Example:
+
+    Html.div []
+        [ Dropdown.dropdown
+            (Dropdown.defaultOptions DropdownChanged)
+            [ class "my-dropdown" ]
+            model.selectedDropdownValue
+        ]
+-}
+dropdown : Options msg -> List (Html.Attribute msg) -> Maybe String -> Html msg
+dropdown options attributes currentValue =
+    let
+        isSelected value =
+            currentValue |> Maybe.map ((==) value) |> Maybe.withDefault False
+
+        toOption { value, text, enabled } =
+            option
+                [ Html.value value
+                , Html.selected (isSelected value)
+                , Html.disabled (not enabled)
+                ]
+                [ Html.text text ]
+
+        itemsWithEmptyItems =
+            case options.emptyItem of
+                Just emptyItem ->
+                    emptyItem :: options.items
+
+                Nothing ->
+                    options.items
+    in
+        select
+            (attributes ++ [ onChange options.emptyItem options.onChange ])
+            (List.map toOption itemsWithEmptyItems)
+
+
+onChange : Maybe Item -> (Maybe String -> msg) -> Html.Attribute msg
+onChange emptyItem tagger =
+    let
+        textToMaybe string =
+            if Maybe.map (.value >> (==) string) emptyItem |> Maybe.withDefault False then
+                Nothing
+            else
+                Just string
+    in
+        on "change" (Decode.map (textToMaybe >> tagger) targetValue)
---
+++ src/DropdownMain.elm
@@ -0,0 +1,85 @@
+module DropdownMain exposing (main)
+
+import Html exposing (Html, text, p, label, form)
+import Html.Attributes exposing (style, for)
+import Html.App as Html
+import Dropdown
+
+
+main : Program Never
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { selectedValue : Maybe String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { selectedValue = Just "2"
+      }
+    , Cmd.none
+    )
+
+
+dropdownOptions : Dropdown.Options Msg
+dropdownOptions =
+    let
+        defaultOptions =
+            Dropdown.defaultOptions DropdownChanged
+    in
+        { defaultOptions
+            | items =
+                [ { value = "1", text = "One", enabled = True }
+                , { value = "2", text = "Two", enabled = True }
+                ]
+            , emptyItem = Just { value = "", text = "[Please Select]", enabled = True }
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    form []
+        [ p []
+            [ label []
+                [ text "Masked Input"
+                , Dropdown.dropdown
+                    dropdownOptions
+                    [ style
+                        [ ( "border", "1px solid #ccc" )
+                        , ( "padding", ".5rem" )
+                        , ( "box-shadow", "inset 0 1px 1px rgba(0,0,0,.075);" )
+                        ]
+                    ]
+                    model.selectedValue
+                ]
+            , text <| Maybe.withDefault "Not Selected" model.selectedValue
+            ]
+        ]
+
+
+type Msg
+    = NoOp
+    | DropdownChanged (Maybe String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        DropdownChanged selectedValue ->
+            ( { model | selectedValue = Debug.log "selectedValue" selectedValue }, Cmd.none )
