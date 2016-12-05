604ef2c0f65d5b4b2ca93c6bfe694728d81d20eb
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2016-12-05 00:30:46 -0600

    Implement onChange

--- src/DatePicker.elm
+++ src/DatePicker.elm
@@ -21,13 +21,15 @@ module DatePicker
 import Date exposing (Date)
 import Html exposing (Html, input, div, span, text, button, table, tr, td, th, thead, tbody)
 import Html.Events exposing (onFocus, onBlur, onClick, onInput)
-import Html.Attributes exposing (class, type_, style)
+import Html.Attributes exposing (class, type_, style, classList)
 import Json.Decode
 import Task
 import DatePicker.Formatter
 import DatePicker.Svg
+import DatePicker.DateUtils
 import Date.Extra.Core
 import Date.Extra.Duration
+import List.Extra
 
 
 -- MODEL
@@ -39,6 +41,7 @@ type alias Options msg =
     , formatter : Date -> String
     , titleFormatter : Date -> String
     , nameOfDays : NameOfDays
+    , firstDayOfWeek : Date.Day
     }
 
 
@@ -72,6 +75,7 @@ defaultOptions onChange toMsg =
     , formatter = DatePicker.Formatter.defaultFormatter
     , titleFormatter = DatePicker.Formatter.titleFormatter
     , nameOfDays = defaultNameOfDays
+    , firstDayOfWeek = Date.Sun
     }
 
 
@@ -122,6 +126,74 @@ getStateValue state =
     case state of
         State stateValue ->
             stateValue
+
+
+
+-- EVENTS
+
+
+onChange : (Maybe Date -> msg) -> Html.Attribute msg
+onChange tagger =
+    Html.Events.on "change" (Json.Decode.map (Date.fromString >> Result.toMaybe >> tagger) Html.Events.targetValue)
+
+
+onMouseDown : msg -> Html.Attribute msg
+onMouseDown msg =
+    let
+        eventOptions =
+            { preventDefault = True
+            , stopPropagation = True
+            }
+    in
+        Html.Events.onWithOptions "mousedown" eventOptions (Json.Decode.succeed msg)
+
+
+onMouseUp : msg -> Html.Attribute msg
+onMouseUp msg =
+    let
+        eventOptions =
+            { preventDefault = True
+            , stopPropagation = True
+            }
+    in
+        Html.Events.onWithOptions "mouseup" eventOptions (Json.Decode.succeed msg)
+
+
+
+-- ACTIONS
+
+
+switchMode : Options msg -> State -> msg
+switchMode options state =
+    let
+        stateValue =
+            getStateValue state
+    in
+        options.toMsg <| State { stateValue | dialogFocused = False, event = "title" }
+
+
+gotoNextMonth : Options msg -> State -> msg
+gotoNextMonth options state =
+    let
+        stateValue =
+            getStateValue state
+
+        updatedTitleDate =
+            Maybe.map (Date.Extra.Duration.add Date.Extra.Duration.Month 1) stateValue.titleDate
+    in
+        options.toMsg <| State { stateValue | dialogFocused = False, event = "next", titleDate = updatedTitleDate }
+
+
+gotoPreviousMonth : Options msg -> State -> msg
+gotoPreviousMonth options state =
+    let
+        stateValue =
+            getStateValue state
+
+        updatedTitleDate =
+            Maybe.map (Date.Extra.Duration.add Date.Extra.Duration.Month -1) stateValue.titleDate
+    in
+        options.toMsg <| State { stateValue | dialogFocused = False, event = "previous", titleDate = updatedTitleDate }
 
 
 
@@ -205,88 +277,72 @@ datePickerDialog options state currentDate =
 calendar : Options msg -> State -> Html msg
 calendar options state =
     let
-        header =
-            thead [ class "days" ]
-                [ tr
-                    []
-                    [ th [] [ text options.nameOfDays.sunday ]
-                    , th [] [ text options.nameOfDays.monday ]
-                    , th [] [ text options.nameOfDays.tuesday ]
-                    , th [] [ text options.nameOfDays.wednesday ]
-                    , th [] [ text options.nameOfDays.thursday ]
-                    , th [] [ text options.nameOfDays.friday ]
-                    , th [] [ text options.nameOfDays.saturday ]
-                    ]
-                ]
-    in
-        table [ class "calendar" ]
-            [ header
-            ]
-
-
-
--- EVENTS
-
-
-onChange : (Maybe Date -> msg) -> Html.Attribute msg
-onChange tagger =
-    Html.Events.on "change" (Json.Decode.map (Date.fromString >> Result.toMaybe >> tagger) Html.Events.targetValue)
-
-
-onMouseDown : msg -> Html.Attribute msg
-onMouseDown msg =
-    let
-        eventOptions =
-            { preventDefault = True
-            , stopPropagation = True
-            }
-    in
-        Html.Events.onWithOptions "mousedown" eventOptions (Json.Decode.succeed msg)
-
-
-onMouseUp : msg -> Html.Attribute msg
-onMouseUp msg =
-    let
-        eventOptions =
-            { preventDefault = True
-            , stopPropagation = True
-            }
-    in
-        Html.Events.onWithOptions "mouseup" eventOptions (Json.Decode.succeed msg)
-
-
-
--- ACTIONS
-
-
-switchMode : Options msg -> State -> msg
-switchMode options state =
-    let
         stateValue =
             getStateValue state
     in
-        options.toMsg <| State { stateValue | dialogFocused = False, event = "title" }
+        case stateValue.titleDate of
+            Nothing ->
+                Html.text ""
 
+            Just titleDate ->
+                let
+                    firstDay =
+                        Date.Extra.Core.toFirstOfMonth titleDate
+                            |> Date.dayOfWeek
+                            |> DatePicker.DateUtils.dayToInt options.firstDayOfWeek
 
-gotoNextMonth : Options msg -> State -> msg
-gotoNextMonth options state =
-    let
-        stateValue =
-            getStateValue state
+                    month =
+                        Date.month titleDate
 
-        updatedTitleDate =
-            Maybe.map (Date.Extra.Duration.add Date.Extra.Duration.Month 1) stateValue.titleDate
-    in
-        options.toMsg <| State { stateValue | dialogFocused = False, event = "next", titleDate = updatedTitleDate }
+                    year =
+                        Date.year titleDate
 
+                    days =
+                        DatePicker.DateUtils.generateCalendar options.firstDayOfWeek month year
 
-gotoPreviousMonth : Options msg -> State -> msg
-gotoPreviousMonth options state =
-    let
-        stateValue =
-            getStateValue state
+                    header =
+                        thead [ class "days-of-week" ]
+                            [ tr
+                                []
+                                [ th [] [ text options.nameOfDays.sunday ]
+                                , th [] [ text options.nameOfDays.monday ]
+                                , th [] [ text options.nameOfDays.tuesday ]
+                                , th [] [ text options.nameOfDays.wednesday ]
+                                , th [] [ text options.nameOfDays.thursday ]
+                                , th [] [ text options.nameOfDays.friday ]
+                                , th [] [ text options.nameOfDays.saturday ]
+                                ]
+                            ]
 
-        updatedTitleDate =
-            Maybe.map (Date.Extra.Duration.add Date.Extra.Duration.Month -1) stateValue.titleDate
-    in
-        options.toMsg <| State { stateValue | dialogFocused = False, event = "previous", titleDate = updatedTitleDate }
+                    toDay day =
+                        td
+                            [ class
+                                (case day.monthType of
+                                    DatePicker.DateUtils.Previous ->
+                                        "previous-month"
+
+                                    DatePicker.DateUtils.Current ->
+                                        "current-month"
+
+                                    DatePicker.DateUtils.Next ->
+                                        "next-month"
+                                )
+                            , onClick <| options.onChange <| Just <| DatePicker.DateUtils.toDate year month day
+                            , onMouseUp <| options.toMsg <| State { stateValue | dialogFocused = False, inputFocused = False, event = "onChange" }
+                            ]
+                            [ text <| toString day.day ]
+
+                    toWeekRow week =
+                        tr [] (List.map toDay week)
+
+                    body =
+                        tbody [ class "days" ]
+                            (days
+                                |> List.Extra.groupsOf 7
+                                |> List.map toWeekRow
+                            )
+                in
+                    table [ class "calendar" ]
+                        [ header
+                        , body
+                        ]
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,9 +2,12 @@ module Tests exposing (..)
 
 import Test exposing (Test, describe)
 import PatternTests
+import DateUtilsTests
 
 
 all : Test
 all =
     describe "All Test Suite"
-        [ PatternTests.all ]
+        [ PatternTests.all
+        , DateUtilsTests.all
+        ]
