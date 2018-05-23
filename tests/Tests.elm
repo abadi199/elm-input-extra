6d2ad4bcd36a8191e9e8fdff35dac36f7fd78e03
Michael Jones <michael@zaptic.com>
2018-05-23 10:56:55 +0100

    Remove date picker tests
    That module has been moved to another project at this point.
    
    The tests still do not run but I'm not sure why.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,12 +2,10 @@ module Tests exposing (..)
 
 import Test exposing (Test, describe)
 import PatternTests
-import DateUtilsTests
 
 
 all : Test
 all =
     describe "All Test Suite"
         [ PatternTests.all
-        , DateUtilsTests.all
         ]
